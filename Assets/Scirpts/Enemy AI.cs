19149dd93b91cb556a497556f6e2f1b9bd4d13b5
AndrewPalangio <andrew.palangio@gmail.com>
2015-11-06 11:51:02 -0500

    AI Shooting

---
+++ Assets/Scirpts/Enemy AI.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public Transform player;
+    public float playerDistance;
+    public float rotationDamping;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerDistance = Vector3.Distance(player.position, transform.position);
+        //Have the enmeny look at the player
+        if (playerDistance < 15.0f)
+        {
+            lookAtPlayer();
+        }
+        if (playerDistance < 12.0f)
+        {
+            chasePlayer();
+        }
+
+    }
+    void lookAtPlayer()
+    {
+        Quaternion rotation = Quaternion.LookRotation(player.position - transform.position);
+        //Rotate the enmey
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * rotationDamping);
+    }
+    void chasePlayer()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
+
+
+
---
+++ Assets/Scirpts/aiShooting.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class aiShooting : MonoBehaviour {
+    public float speed = 30;
+    public Rigidbody projectiles;
+    public GameObject gun;
+    public Rigidbody player;
+    RaycastHit hit;
+
+
+    public void shoot(){
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(gun.transform.position, fwd * 2, out hit, 20f))
+        {
+            if (hit.collider.tag.Equals("Player"))
+            {
+                Rigidbody instantiateProjectiles = Instantiate(projectiles, gun.transform.position, gun.transform.rotation) as Rigidbody;
+                instantiateProjectiles.velocity = transform.TransformDirection(new Vector3(0, 0, speed));
+            }
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
